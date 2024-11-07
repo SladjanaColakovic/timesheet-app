640a389601e4ed96f159f1be917e78c9101cb361
Sladjana Colakovic <Vladimir.Sobic@dualgroup.com>
2024-11-07 15:39:12 +0100

    Project setup and DB connction

---
+++ app/src/main/java/com/timesheet/app/model/Category.java
@@ -0,0 +1,19 @@
+package com.timesheet.app.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+}
