1d90636c8228902ba434b0c1319246ad15237492
Sladjana Colakovic <Vladimir.Sobic@dualgroup.com>
2024-11-13 13:14:39 +0100

    Refactor report feature

--- app/src/main/java/com/timesheet/app/repository/TimesheetItemRepository.java
+++ app/src/main/java/com/timesheet/app/repository/TimesheetItemRepository.java
@@ -15,15 +15,14 @@ import java.util.List;
 @Repository
 public interface TimesheetItemRepository extends JpaRepository<TimesheetItem, Long> {
     List<TimesheetItem> findByEmployeeIdAndDate(Long employeeId, LocalDate date);
-    @Query("SELECT new com.timesheet.app.helper.ReportItem(i.date, i.employee, i.project, i.client, i.category, i.description, sum(i.hours + i.overtime)) " +
+    @Query("SELECT new com.timesheet.app.helper.ReportItem(i.date, i.employee, i.project, i.client, i.category, i.description, i.hours + i.overtime) " +
             "FROM TimesheetItem i " +
             "WHERE (COALESCE(:#{#reportParams.startDate}, null) IS null OR i.date >= :#{#reportParams.startDate}) AND" +
             "(COALESCE(:#{#reportParams.endDate}, null) IS null OR i.date <= :#{#reportParams.endDate}) AND" +
             "(:#{#reportParams.employeeId} IS null OR :#{#reportParams.employeeId} = i.employee.id) AND" +
             "(:#{#reportParams.projectId} IS null OR :#{#reportParams.projectId} = i.project.id) AND" +
             "(:#{#reportParams.clientId} IS null OR :#{#reportParams.clientId} = i.client.id) AND" +
-            "(:#{#reportParams.categoryId} IS null OR :#{#reportParams.categoryId} = i.category.id)" +
-            "GROUP BY i.date, i.employee, i.project, i.client, i.category, i.description")
+            "(:#{#reportParams.categoryId} IS null OR :#{#reportParams.categoryId} = i.category.id)")
     List<ReportItem> getTimesheetReport(ReportSearchParams reportParams);
 
     @Query("SELECT new com.timesheet.app.helper.MonthlyTimesheetItem(i.date, sum(i.hours + i.overtime)) " +
