94c521392b4575c690c4a70998b6bf78674f2456
Sladjana Colakovic <danilo.knezevic@dualgroup.com>
2024-11-22 16:23:30 +0100

    Add Tests for ReportService class

---
+++ app/src/test/java/com/timesheet/app/data/ReportServiceConstants.java
@@ -0,0 +1,11 @@
+package com.timesheet.app.data;
+
+import java.time.LocalDate;
+
+public class ReportServiceConstants {
+    public static final String REPORT_ITEM_DESCRIPTION = "Test Description";
+    public static final LocalDate DATE = LocalDate.of(2024, 11, 12);
+    public static final double TIME = 5;
+    public static final int LIST_SIZE = 2;
+    public static final double TOTAL_HOURS = 10;
+}
